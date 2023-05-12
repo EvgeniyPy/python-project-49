e39daa74894d11ff71baee356617bd9acc7745fe
EvgeniyPy <evgeniy.artamonov@yahoo.com>
2023-05-12 19:38:30 +0300

    fix_errors10

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -3,16 +3,18 @@ from operator import add, sub, mul
 
 RULE_OF_GAME = 'What is the result of the expression?'
 
+OPERATION = [("+", add), ("-", sub), ("*", mul)]
 
-def lets_play():
+
+def get_game():
     '''
     Функция генерирует случайное арифметическое выражаение.
     Запрашивает ответ у пользователя. Возвращает результат
     '''
-    a = randint(1, 10)
-    b = randint(1, 10)
-    operation = [("+", add), ("-", sub), ("*", mul)]
-    operator, func = choice(operation)  # tuple(str, Callable)
-    correct_answer = str(func(a, b))
-    question = f'{a} {operator} {b}'
+    random_number_1 = randint(1, 10)
+    random_number_2 = randint(1, 10)
+
+    operator, func = choice(OPERATION)  # tuple(str, Callable)
+    correct_answer = str(func(random_number_1, random_number_2))
+    question = f'{random_number_1} {operator} {random_number_2}'
     return correct_answer, question
