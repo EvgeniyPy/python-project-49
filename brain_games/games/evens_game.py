30272e2b5d732db670dbf353b17065a3662933f4
EvgeniyPy <evgeniy.artamonov@yahoo.com>
2023-05-12 20:52:03 +0300

    fix_errors

--- brain_games/games/evens_game.py
+++ brain_games/games/evens_game.py
@@ -4,13 +4,13 @@ RULE_OF_GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def is_even(number):
-   if number % 2 == 0:
-       return 'yes'
-   else:
-       return 'no'
+    return number % 2 == 0
 
 
 def get_game() -> str:
     number = randint(1, 100)
-    correct_answer = is_even(number)
+    if is_even(number):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
     return correct_answer, number
