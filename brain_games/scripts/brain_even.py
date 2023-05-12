643b1474ae2184b25895de1ebab4e21535d4406f
EvgeniyPy <evgeniy.artamonov@yahoo.com>
2023-05-12 20:18:57 +0300

    fix_errors

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,9 @@
-from brain_games.games import even_game
+from brain_games.games import evens_game
 from brain_games.game_engine import launch_game
 
 
 def main():
-    launch_game(even_game)
+    launch_game(evens_game)
 
 
 if __name__ == '__main__':
