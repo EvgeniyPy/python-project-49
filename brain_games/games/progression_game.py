ff717756050ef2e2d77a44ed8b70642aa3319986
EvgeniyPy <evgeniy.artamonov@yahoo.com>
2023-05-13 18:13:30 +0300

    last_fix

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -5,18 +5,20 @@ RULE_OF_GAME = "What number is missing in the progression?"
 
 
 def get_random_progression():
-    pass
-
-
-def get_game():
-    """Функция создае рандомную прогресиию
-    и возвращает ее с пропущенным элементом(...)"""
     start = randint(1, 50)
     step = randint(2, 7)
     length = randint(5, 10)
     _progression = list(range(start, start + step * length, step))
-    random_index = randint(0, len(_progression) - 1)
+    return _progression
 
+
+def get_game() -> tuple[str, str]:
+    """
+    Функция создае рандомную прогресиию
+    и возвращает ее с пропущенным элементом(...)
+    """
+    _progression = get_random_progression()
+    random_index = randint(0, len(_progression) - 1)
     correct_answer = _progression[random_index]
     _progression[random_index] = '..'
     question = " ".join(map(str, _progression))
